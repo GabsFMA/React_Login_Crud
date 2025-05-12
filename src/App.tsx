813be460cd2cc8b104917a16262ac93ff35a73d0
Gabriel Francisco <105892017+GabsFMA@users.noreply.github.com>
2025-05-12 12:24:37 -0300

    feat: integrating loginPage routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/loginPage";
+import IntroductionPage from "./pages/introductionPage";
+
 function App() {
   return (
-    <div className="relative w-screen h-screen overflow-hidden">
-      <div
-        className="absolute inset-0 bg-cover bg-center bg-no-repeat brightness-50"
-        style={{ backgroundImage: "url('/background.jpg')" }}
-      />
-      <div className="relative z-10 flex items-center justify-center h-full">
-        <img
-          src="/logo.png"
-          alt="logo"
-          className="w-4/5 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg"
-        />
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<IntroductionPage />} />
+      <Route path="/loginPage" element={<LoginPage />} />
+    </Routes>
   );
 }
 
-export default App
+export default App;
---
+++ src/pages/loginPage.tsx
@@ -0,0 +1,16 @@
+function loginPage() {
+  return (
+    <div className="bg-[#1E1A1A] w-screen h-screen flex items-center justify-center">
+        <div className="w-md h-md flex items-center justify-center">
+            <img src="/logo.png" alt="logo" />
+        </div>
+        <div>
+            <form className="flex flex-col items-center justify-center">
+            </form>
+        </div>
+        <div></div>
+    </div>
+  );
+}
+
+export default loginPage;
