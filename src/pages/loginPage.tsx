8796d0e066c002902cc3068d4c76adcb8d62df7d
Gabriel Francisco <105892017+GabsFMA@users.noreply.github.com>
2025-05-12 22:49:16 +0000

    feat: add form tag

--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -1,11 +1,28 @@
 function loginPage() {
   return (
-    <div className="bg-[#1E1A1A] w-screen h-screen flex items-center justify-center">
-        <div className="w-md h-md flex items-center justify-center">
+    <div className="bg-[#1E1A1A] w-screen h-screen flex flex-col items-center justify-center">
+        <div className="w-80 h-80 flex items-center justify-center">
             <img src="/logo.png" alt="logo" />
         </div>
-        <div>
+        <div className="flex flex-col">
             <form className="flex flex-col items-center justify-center">
+              <div className="flex flex-col items-center justify-center gap-4">
+                <input
+                  type="text"
+                  className="bg-[#373232] w-80 h-12 rounded-md text-center text-white"
+                  placeholder="E-mail"
+                />
+                <input
+                  type="password"
+                  className="bg-[#373232] w-80 h-12 rounded-md text-center text-white"
+                  placeholder="Senha"
+                />
+              </div>
+              <div>
+                <button className="bg-[#E80000] w-80 h-12 rounded-md text-center text-white mt-4">
+                  Entrar
+                </button>
+              </div>
             </form>
         </div>
         <div></div>
